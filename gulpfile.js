900f8873ff8093cc0754f2a0ad62f198a2395078
Andrew Miller <avmiller@outlook.com>
2015-05-16 09:10:06 -0400

    Enabling live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-var gulp = require('gulp'), connect = require('gulp-connect');
+var gulp = require('gulp'), 
+    connect = require('gulp-connect'), 
+    livereload = require('gulp-livereload'), 
+    watch = require('gulp-watch');
 
 gulp.task('webserver', function() {
     connect.server();
+});
+
+gulp.task('livereload', function() {
+    connect.reload();
+    livereload.reload();
+});
+
+gulp.task('watch', function() {
+    connect.server();
+    livereload.listen();
+    gulp.watch('src/**/*.js', [ 'livereload' ]);
 });
 
 gulp.task('default', [ 'webserver' ]);
